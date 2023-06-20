8c9211276424f71f8168b5def05807d78a6727ef
Ami Eaton <ami.eaton@pearson.com>
2023-06-19 21:52:35 -0700

    initial api

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,46 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
+const Home = () => {
+  useEffect(async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://real-time-finance-data.p.rapidapi.com/search',
+      params: {
+        query: 'Apple',
+        language: 'en'
+      },
+      headers: {
+        'X-RapidAPI-Key': '0400ba954bmshc50b5f7c9681c1dp192547jsn8d988983d08e',
+        'X-RapidAPI-Host': 'real-time-finance-data.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      setData(response.data)
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const [data, setData] = useState();
+
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>News For Stock A</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          {data}
         </p>
       </main>
 
@@ -21,3 +48,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
